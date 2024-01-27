bbbfe4f35196dc5f365ed7f60ed5038d8debdc16
Quexiclact <156546364+Quexiclact@users.noreply.github.com>
2024-01-26 22:05:36 -0500

    Add files via upload

--- formatting.js
+++ formatting.js
@@ -1,15 +1,24 @@
 /**
  * Sets theme based on preference
- */
+ *
 window.onload = function(){
     const prefersDarkTheme = window.matchMedia('(prefers-color-scheme: dark)');
     if (prefersDarkTheme.matches) {
         document.getElementById("work").classList.add("reversi");
     }
     else {
-	document.getElementById("work").classList.add("light");
+		document.getElementById("work").classList.add("light");
+	}
+}; */
+document.addEventListener("DOMContentLoaded", function(arg) {
+    const prefersDarkTheme = window.matchMedia('(prefers-color-scheme: dark)');
+    if (prefersDarkTheme.matches) {
+        document.getElementById("work").classList.add("reversi");
     }
-};
+    else {
+		document.getElementById("work").classList.add("light");
+	}
+});
 
 /**
  * toggles theme from default to reversi and back
